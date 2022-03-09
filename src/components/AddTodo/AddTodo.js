f694ca9348da9d4308048ecb9e54aa1f82fcb7ea
Brham Dev Mahato <brham1980@gmail.com>
2022-03-09 22:29:55 +0545

    fetch data from api

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { addTodo } from '../../actions';
+
 import cuid from 'cuid';
 import axios from 'axios';
 
 const AddTodo = () => {
   const [tasks, setTasks] = useState('');
   const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos);
+  console.log(todos.data);
+  console.log(todos);
   function handleInput(e) {
     setTasks(e.target.value);
     console.log(tasks);
@@ -18,6 +22,23 @@ const AddTodo = () => {
     console.log(tasks);
   }
 
+  const getTodos = async () => {
+    const response = await axios
+      .get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .catch((err) => {
+        console.error('Err', err);
+      });
+    let data = response.data;
+    data.map((todo) => {
+      let { title, id } = todo;
+      return dispatch(addTodo({ task: title, id: id }));
+    });
+  };
+  useEffect(() => {
+    if (todos.data.length <= 0) {
+      getTodos();
+    }
+  }, []);
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" name="userInput" onChange={(e) => handleInput(e)} />
